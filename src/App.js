fcb97cea32dbe6128c1ab453f88a2caa342b4450
Quandiq <112625513+quandiq02@users.noreply.github.com>
2023-02-27 18:58:03 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import Fifth from "./components/fifth-task/fifth";
 import First from "./components/first-task/First";
 import Forth from "./components/forth-task/forth";
 import Second from "./components/second-task/Second";
+import Seventh from "./components/seventh-task/seventh";
+import Sixth from "./components/sixth-task/sixth";
 import Third from "./components/third-task/Third";
 
 function App() {
   return (
     <div className="body-wrapper">
       <First />
-      <Second/>
-      <Third/>
-      <Forth/>
-      <Fifth/>
+      <Second />
+      <Third />
+      <Forth />
+      <Fifth />
+      <Sixth />
+      <Seventh />
     </div>
   );
 }
