11674ea92ac0849838cc8cb9c2a80de279e0c929
Quandiq <112625513+quandiq02@users.noreply.github.com>
2023-03-03 18:51:52 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import Thirteen from "./components/13-task/13";
+import Fourteen from "./components/14-task/14";
+import Eigth from "./components/eigth-task/eigth";
+import Eleventh from "./components/eleventh-task/eleventh";
 import Fifth from "./components/fifth-task/fifth";
 import First from "./components/first-task/First";
 import Forth from "./components/forth-task/forth";
+import Ninth from "./components/ninth-task/ninth";
 import Second from "./components/second-task/Second";
 import Seventh from "./components/seventh-task/seventh";
 import Sixth from "./components/sixth-task/sixth";
+import Tenth from "./components/tenth-task/tenth";
 import Third from "./components/third-task/Third";
+import Twelfth from "./components/twelfth-task/twelfth";
 
 function App() {
   return (
@@ -16,6 +23,13 @@ function App() {
       <Fifth />
       <Sixth />
       <Seventh />
+      <Eigth />
+      <Ninth/>
+      <Tenth/>
+      <Eleventh/>
+      <Twelfth/>
+      <Thirteen/>
+      <Fourteen/>
     </div>
   );
 }
