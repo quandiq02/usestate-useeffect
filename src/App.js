28b2a087f32ea5ca5cfa2b3add09dd82aef7c802
Quandiq <112625513+quandiq02@users.noreply.github.com>
2023-03-10 18:56:14 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
-import Thirteen from "./components/13-task/13";
-import Fourteen from "./components/14-task/14";
-import Eigth from "./components/eigth-task/eigth";
-import Eleventh from "./components/eleventh-task/eleventh";
-import Fifth from "./components/fifth-task/fifth";
-import First from "./components/first-task/First";
-import Forth from "./components/forth-task/forth";
-import Ninth from "./components/ninth-task/ninth";
-import Second from "./components/second-task/Second";
-import Seventh from "./components/seventh-task/seventh";
-import Sixth from "./components/sixth-task/sixth";
-import Tenth from "./components/tenth-task/tenth";
-import Third from "./components/third-task/Third";
-import Twelfth from "./components/twelfth-task/twelfth";
+import Card from "./components/Others/card/card";
+import Modal from "./components/Others/Modal/modal";
+import Car from "./components/useReducer/car/car";
+import Counter from "./components/useReducer/counter/counter";
+import Reset from "./components/useReducer/reset/reset";
+import TodoList from "./components/useReducer/todo/todo";
 
 function App() {
   return (
     <div className="body-wrapper">
-      <First />
-      <Second />
-      <Third />
-      <Forth />
-      <Fifth />
-      <Sixth />
-      <Seventh />
-      <Eigth />
-      <Ninth/>
-      <Tenth/>
-      <Eleventh/>
-      <Twelfth/>
-      <Thirteen/>
-      <Fourteen/>
+      <Counter />
+      <TodoList />
+      <Reset />
+      <Modal />
+      <Car/>
+      {/* <Card/> */}
     </div>
   );
 }
