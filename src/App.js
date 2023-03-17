c0d8da63d22b7ba680ccaf7cb45dea3cd7f596ca
Quandiq <112625513+quandiq02@users.noreply.github.com>
2023-03-17 19:05:23 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import Card from "./components/Others/card/card";
+import Disconnect from "./components/Others/disconnect/disconnet";
 import Modal from "./components/Others/Modal/modal";
+import RouteFirst from "./components/routes/route-first/pages/Main/main-route";
+import RouteSecond from "./components/routes/route-second/pages/Sympsons/sympson-route";
 import Car from "./components/useReducer/car/car";
 import Counter from "./components/useReducer/counter/counter";
 import Reset from "./components/useReducer/reset/reset";
@@ -8,12 +11,15 @@ import TodoList from "./components/useReducer/todo/todo";
 function App() {
   return (
     <div className="body-wrapper">
-      <Counter />
+      {/* <Counter />
       <TodoList />
       <Reset />
       <Modal />
-      <Car/>
-      {/* <Card/> */}
+      <Car/> */}
+      <Card />
+      <Disconnect />
+      <RouteFirst />
+      <RouteSecond />
     </div>
   );
 }
